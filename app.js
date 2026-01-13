fe408693e86debc2010ecff44a0850f28f57f4ed
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-13 18:22:26 +0530

    init and Globals in Node

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log("Hello World!");
+
+// Globals - In Node.js, these are not attached to the window object like in browsers
+// let count = 0;
+
+// const timer = setInterval(() => {
+//   count = count + 2;
+//   console.log(`${count} seconds have passed!`);
+//   if (count > 6) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
+
+// console.log(__dirname);
+console.log(__filename);
