14fdc3160176e4772ba37c61339a01767908dd20
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-13 20:16:05 +0530

    Module - Event Emitter

--- recap.js
+++ recap.js
@@ -31,10 +31,44 @@
 
 // callBackFunc(blue);
 
-// Modules - In Node.js, each file is treated as a separate module
-const helper = require("./helper");
+// // Modules - In Node.js, each file is treated as a separate module
+// const helper = require("./helper");
 
 // console.log(helper("John"));
-console.log(helper.user("John"));
-console.log(helper.id("123"));
-console.log(helper.email("john@mail.co"));
+// console.log(helper.user("John"));
+// console.log(helper.id("123"));
+// console.log(helper.email("john@mail.co"));
+
+// Event Emitter - Node.js has a built-in module called 'events' that allows us to work with events. And, 'util' module helps to inherit the event properties.
+const events = require("events");
+const util = require("util");
+
+// const myEmitter = new events.EventEmitter;
+
+// myEmitter.on('test', function (arg) {
+//     console.log(arg);
+// })
+
+// myEmitter.emit('test', 'Hello World!')
+
+const teams = function (name) {
+  this.name = name;
+};
+
+util.inherits(teams, events.EventEmitter);
+
+const Liverpool = new teams("Liverpool");
+const Juventues = new teams("Juventues");
+const Porto = new teams("Porto");
+
+const teamArray = [Liverpool, Juventues, Porto];
+
+teamArray.forEach((team) => {
+  team.on("nation", function (nation) {
+    console.log(team.name + " is " + nation + " football club.");
+  });
+});
+
+Liverpool.emit("nation", "English");
+Juventues.emit("nation", "Italian");
+Porto.emit("nation", "Portugese");
