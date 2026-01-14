fa6fde95f54da95683d3e1519724d33095cc2a66
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-14 18:25:24 +0530

    Read and write files

--- recap.js
+++ recap.js
@@ -39,9 +39,9 @@
 // console.log(helper.id("123"));
 // console.log(helper.email("john@mail.co"));
 
-// Event Emitter - Node.js has a built-in module called 'events' that allows us to work with events. And, 'util' module helps to inherit the event properties.
-const events = require("events");
-const util = require("util");
+// // Event Emitter - Node.js has a built-in module called 'events' that allows us to work with events. And, 'util' module helps to inherit the event properties.
+// const events = require("events");
+// const util = require("util");
 
 // const myEmitter = new events.EventEmitter;
 
@@ -51,24 +51,73 @@ const util = require("util");
 
 // myEmitter.emit('test', 'Hello World!')
 
-const teams = function (name) {
-  this.name = name;
-};
+// const teams = function (name) {
+//   this.name = name;
+// };
 
-util.inherits(teams, events.EventEmitter);
+// util.inherits(teams, events.EventEmitter);
 
-const Liverpool = new teams("Liverpool");
-const Juventues = new teams("Juventues");
-const Porto = new teams("Porto");
+// const Liverpool = new teams("Liverpool");
+// const Juventues = new teams("Juventues");
+// const Porto = new teams("Porto");
 
-const teamArray = [Liverpool, Juventues, Porto];
+// const teamArray = [Liverpool, Juventues, Porto];
 
-teamArray.forEach((team) => {
-  team.on("nation", function (nation) {
-    console.log(team.name + " is " + nation + " football club.");
+// teamArray.forEach((team) => {
+//   team.on("nation", function (nation) {
+//     console.log(team.name + " is " + nation + " football club.");
+//   });
+// });
+
+// Liverpool.emit("nation", "English");
+// Juventues.emit("nation", "Italian");
+// Porto.emit("nation", "Portugese");
+
+// // Read and Write Files - Node.js has a built-in module called 'fs' to work with the file system
+// const fs = require("fs");
+
+// Read File - synchronous and encoding specified
+// const text = fs.readFileSync("read-me.txt", "utf-8");
+// console.log(text);
+
+// Write File
+// fs.writeFileSync("write.txt", text);
+
+// Read File - asynchronous
+// fs.readFile("read-me.txt", "utf-8", function (err, data) {
+//   console.log(data);
+// });
+
+// Write File - asynchronous
+// fs.readFile("read-me.txt", "utf-8", function (err, data) {
+//   fs.writeFile("write.txt", data, function (err) {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
+// });
+
+// Delete File
+// fs.unlink("write.txt", (err) => {
+//   console.log(err);
+// });
+
+// // Create and Remove Directory
+const fs = require("fs");
+
+// Synchronous
+// fs.mkdirSync("NewFolder");
+// fs.rmdirSync("NewFolder");
+
+// Asynchronous
+// fs.mkdir("NewFolder", () => {
+//   fs.readFile("read-me.txt", "utf-8", (err, data) => {
+//     fs.writeFile("./NewFolder/write.txt", data, () => {});
+//   });
+// });
+
+fs.unlink("./NewFolder/write.txt", () => {
+  fs.rmdir("NewFolder", (err) => {
+    console.log(err);
   });
 });
-
-Liverpool.emit("nation", "English");
-Juventues.emit("nation", "Italian");
-Porto.emit("nation", "Portugese");
