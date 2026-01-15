4af7ad79efb2855f189082dd872e04d61aecd154
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-15 18:27:52 +0530

    Streams & Buffers

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+// // Server Using Node.js
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+
+//   res.writeHead(200, { "content-type": "text/plain" });
+//   res.end("Hello World!");
+// });
+
+// console.log("Port 3000");
+
+// server.listen(3000, "127.0.0.1");
+
+// Streams and Buffers
 const http = require("http");
+const fs = require("fs");
+
+// const readStream = fs.createReadStream(__dirname + "/read-me.txt", "utf-8");
+// const writeStream = fs.createWriteStream(__dirname + "/write-me.txt");
+
+// readStream.on("data", (chunk) => {
+//   console.log("New data received!");
+//   console.log(chunk);
+//   writeStream.write(chunk);
+// });
+
+// readStream.pipe(writeStream);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  // res.writeHead(200, { "content-type": "text/html" });
+  // const readStream = fs.createReadStream(__dirname + "/index.html", "utf-8");
 
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Hello World!");
+  // readStream.pipe(res);
+  res.writeHead(200, { "content-type": "application/json" });
+  const person = {
+    name: "Aladdin",
+    email: "aladdin@gmail.com",
+    job: "Adventurer",
+  };
+
+  res.end(JSON.stringify(person));
 });
-
-console.log("Port 3000");
 
 server.listen(3000, "127.0.0.1");
